4a1c390f8097d69ea180450a78f83fe07043e27a
Zac-L <Zachlichens@gmail.com>
2018-03-05 18:10:24 -0800

    added sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    // add your plugins here
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ],
 };
