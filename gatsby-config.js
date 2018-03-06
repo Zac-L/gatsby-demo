e95df0a09e39a056e5054d8ec569ec4bf1a7461f
Zac-L <Zachlichens@gmail.com>
2018-03-05 18:48:13 -0800

    brought in transformer and source filesystem. Setup options for source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,16 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
-    // add your plugins here
+    // add plugins here
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      }
+    },
   ],
 };
